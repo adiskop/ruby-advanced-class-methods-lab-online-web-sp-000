4ba87c2120a7174c5c957de11f46f3bb270e7a5f
Adi Skop <adiskop@gmail.com>
2020-07-16 14:44:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,27 +43,27 @@ def self.alphabetical
 @@all.sort_by{|song|song.name}
 end
 
-def self.new_from_filename(filename)
-  split_filename= filename.chomp(".mp3").split(" - ")
-  #split_filename= filename.chop.chop.chop.chop(".mp3").split(" - ")
-  song= Song.new
-  song.name= split_filename[1]
-  song.artist_name= split_filename[0]
+  def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
 
-  song
-end
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
 
-def self.create_from_filename(filename)
- parts = filename.split(" - ")
- artist_name = parts[0]
- song_name = parts[1].gsub(".mp3", "")
+  def self.create_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
 
- song = self.create
- song.name = song_name
- song.artist_name = artist_name
- song
-end
-
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
 
 
 
