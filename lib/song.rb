5d8c0c63a1ef8f188f7a613177aab2b9d1d1169a
Adi Skop <adiskop@gmail.com>
2020-07-16 14:40:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,24 @@ def self.new_from_filename(filename)
   song= Song.new
   song.name= split_filename[1]
   song.artist_name= split_filename[0]
+
   song
+end
+
+def self.create_from_filename(filename)
+  split_filename= filename.chomp(".mp3").split(" - ")
+  #split_filename= filename.chop.chop.chop.chop(".mp3").split(" - ")
+  song= Song.new
+  song.name= split_filename[1]
+  song.artist_name= split_filename[0]
+  song.save
+  song
+  
+  
+end
+
+def self.destroy_all
+  
 end
 
 
